a9dfe344646972da51358e9b0225a18042d86092
Jesse Mucheke <jmucheke274@gmail.com>
2024-10-30 12:37:45 +0300

    changed some code

--- movie-app/js/script.js
+++ movie-app/js/script.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ec225067d5b734095c479ae5d3a79bd1&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=&page=1'
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=ec225067d5b734095c479ae5d3a79bd1&query='
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=&query='
 
 
 const main = document.getElementById('main')
